621f2466f4110bf71744183a9193c992ea25027a
Takasur Azeem <takasurazeem@gmail.com>
2022-05-08 11:43:42 +0500

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Takasur Azeem on 08/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
