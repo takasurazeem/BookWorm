da91ed262eba319de9b99d9be605c1efb233fb44
Takasur Azeem <takasurazeem@gmail.com>
2022-05-09 07:23:47 +0500

    Building a list with @FetchRequest
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

---
+++ BookWorm/EmojiRatingView.swift
@@ -0,0 +1,34 @@
+//
+//  EmojiRatingView.swift
+//  BookWorm
+//
+//  Created by Takasur Azeem on 09/05/2022.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    
+    let rating: Int16
+    
+    var body: some View {
+        switch rating {
+        case 1:
+            Text("1")
+        case 2:
+            Text("2")
+        case 3:
+            Text("3")
+        case 4:
+            Text("4")
+        default:
+            Text("5")
+        }
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView(rating: 3)
+    }
+}
